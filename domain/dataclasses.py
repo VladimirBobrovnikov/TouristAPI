289053436114257a71fd16ed254bd3fcba0a96a9
BobrovnikovVladimir <t9126362257@gmail.com>
2022-09-01 07:28:01 +0500

    Добавил примеры запросов в сваггер, расштрил ReadMe

--- domain/dataclasses.py
+++ domain/dataclasses.py
@@ -115,6 +115,95 @@ class PerevalInfo(BodyInfo):
     status: bool = Field(description='Статус прохождения модерации', example=False)
 
 
+class BaseResponse(BaseModel):
+    status: int = Field(description='Статус выполнения HTTP запроса', example=201)
+    message: Optional[str] = Field(
+        default=None,
+        description='Сообщение об ошибке, если произошла',
+        example='TypeError'
+    )
+
+
+class PerevalAddedResponse(BaseResponse):
+    id: Optional[int] = Field(default=None, description='ID созданной записи', example=2)
+
+
+class PerevalUpdateResponse(BaseResponse):
+    state: Optional[int] = Field(default=None, description='Статус обновления записи', example=1)
+
+
+class PerevalsByUserResponse(BaseResponse):
+    pereval_data: List[PerevalInfoResponse] = Field(
+        description='Коллекция сгруппированных данных по перевалам, добавленным пользователем',
+        example=[
+            {
+                'id': 1,
+                'beauty_title': 'пер. ',
+                'title': 'Пхия',
+                'other_titles': 'Триев',
+                'connect': 'Текстовое поле',
+                'add_time': datetime.utcnow().isoformat(),
+                'coords': {
+                    'latitude': 45.3842,
+                    'longitude': 7.1525,
+                    'height': 1200
+                },
+                'level': {
+                    'winter': '2B',
+                    'summer': '1А',
+                    'autumn': '1А',
+                    'spring': '2B',
+                },
+                'images': [
+                    {
+                        'img': '\\x8534234568...',
+                        'title': 'Седловина'
+                    }
+                ],
+                'status': False
+            }
+        ]
+    )
+
+
+class PerevalByIdResponse(BaseResponse):
+    pereval_data: Optional[BodyInfo] = Field(
+        default=None,
+        description='Данные по перевалу',
+        example={
+            'beauty_title': 'пер. ',
+            'title': 'Пхия',
+            'other_titles': 'Триев',
+            'connect': 'Текстовое поле',
+            'add_time': datetime.utcnow().isoformat(),
+            'user': {
+                'email': 'qwerty@mail.ru',
+                'fam': 'Пупкин',
+                'name': 'Василий',
+                'otc': 'Иванович',
+                'phone': '+7 555 55 55'
+            },
+            'coords': {
+                'latitude': 45.3842,
+                'longitude': 7.1525,
+                'height': 1200
+            },
+            'level': {
+                'winter': '2B',
+                'summer': '1А',
+                'autumn': '1А',
+                'spring': '2B',
+            },
+            'images': [
+                {
+                    'img': '\\x8534234568...',
+                    'title': 'Седловина'
+                }
+            ],
+        }
+    )
+
+
 @dataclass
 class User:
     phone: str
